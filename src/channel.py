560cabdcacacf58ef739191dfb8483e9a66d6bfa
Eugene2605 <nojd2605@gmail.com>
2023-07-19 21:55:03 +0300

    Add def str, def add, def sud, def gt, def ge, def lt, def le, def eq in channel.py (homework-3)

--- src/channel.py
+++ src/channel.py
@@ -17,10 +17,41 @@ class Channel:
         self.title = self.channel['items'][0]['snippet']['title']
         self.description = self.channel['items'][0]['snippet']['description']
         self.url = 'https://www.youtube.com/channel/' + self.__channel_id
-        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
-        self.video_count = self.channel['items'][0]['statistics']['videoCount']
-        self.view_count = self.channel['items'][0]['statistics']['viewCount']
+        self.subscriber_count = int(self.channel['items'][0]['statistics']['subscriberCount'])
+        self.video_count = int(self.channel['items'][0]['statistics']['videoCount'])
+        self.view_count = int(self.channel['items'][0]['statistics']['viewCount'])
 
+    def __str__(self):
+        """Возвращает название и ссылку на канал по шаблону <название_канала> (<ссылка_на_канал>)"""
+        return f'{self.title} ({self.url})'
+
+    def __add__(self, other):
+        """Сложение количества подписчиков двух каналов (self + other)"""
+        return self.subscriber_count + other.subscriber_count
+
+    def __sub__(self, other):
+        """Вычитание количества подписчиков двух каналов (self - other)"""
+        return self.subscriber_count - other.subscriber_count
+
+    def __gt__(self, other):
+        """Сравнение количества подписчиков двух каналов (self > other)"""
+        return self.subscriber_count > other.subscriber_count
+
+    def __ge__(self, other):
+        """Сравнение количества подписчиков двух каналов (self >= other)"""
+        return self.subscriber_count >= other.subscriber_count
+
+    def __lt__(self, other):
+        """Сравнение количества подписчиков двух каналов (self < other)"""
+        return self.subscriber_count < other.subscriber_count
+
+    def __le__(self, other):
+        """Сравнение количества подписчиков двух каналов (self <= other)"""
+        return self.subscriber_count <= other.subscriber_count
+
+    def __eq__(self, other):
+        """Сравнение количества подписчиков двух каналов (self == other)"""
+        return self.subscriber_count == other.subscriber_count
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
