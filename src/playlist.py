081481a4a5467a32886bef7f31ddf9bacafeea92
Eugene2605 <nojd2605@gmail.com>
2023-08-02 21:52:12 +0300

    Apgrade file playlist.py and video.py

--- src/playlist.py
+++ src/playlist.py
@@ -1,4 +1,3 @@
-import json
 import os
 from datetime import timedelta
 import isodate as isodate
@@ -9,8 +8,10 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class PlayList:
+    """Класс для ютуб-плейлиста"""
 
-    def __init__(self, playlist_id):
+    def __init__(self, playlist_id: str) -> None:
+        """Экземпляр инициализируется id плейлиста."""
         self.playlist_id = playlist_id
         self.playlist = youtube.playlists().list(id='PLv_zOGKKxVpj-n2qLkEM2Hj96LO6uqgQw',
                                                  part='contentDetails,snippet',
@@ -19,8 +20,8 @@ class PlayList:
         self.title = self.playlist['items'][0]['snippet']['title']
         self.url = 'https://www.youtube.com/playlist?list=' + playlist_id
 
-    @property
-    def total_duration(self):
+    def youtube_requests(self):
+        """Возвращает данные по видеороликам в плейлисте"""
         playlist_videos = youtube.playlistItems().list(playlistId=self.playlist_id,
                                                        part='contentDetails',
                                                        maxResults=50,
@@ -29,6 +30,12 @@ class PlayList:
         video_response = youtube.videos().list(part='contentDetails,statistics',
                                                id=','.join(video_ids)
                                                ).execute()
+        return video_response
+
+    @property
+    def total_duration(self):
+        """Возвращает объект класса `datetime.timedelta` с суммарной длительность плейлиста"""
+        video_response = self.youtube_requests()
         total_duration = timedelta(0)
         for video in video_response['items']:
             # YouTube video duration is in ISO 8601 format
@@ -38,14 +45,8 @@ class PlayList:
         return total_duration
 
     def show_best_video(self):
-        playlist_videos = youtube.playlistItems().list(playlistId=self.playlist_id,
-                                                       part='contentDetails',
-                                                       maxResults=50,
-                                                       ).execute()
-        video_ids = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
-        video_response = youtube.videos().list(part='contentDetails,statistics',
-                                               id=','.join(video_ids)
-                                               ).execute()
+        """Возвращает ссылку на самое популярное видео из плейлиста (по количеству лайков)"""
+        video_response = self.youtube_requests()
         like_count = 0
         best_video_id = ''
         for video in video_response['items']:
--- src/video.py
+++ src/video.py
@@ -5,6 +5,11 @@ api_key = os.getenv('YOUTUBE_API_KEY')
 youtube = build('youtube', 'v3', developerKey=api_key)
 
 
+class IdError(Exception):
+    """Класс исключения при передаче несуществующего id видео"""
+    pass
+
+
 class Video:
     """Класс для ютуб-видео"""
 
@@ -14,10 +19,19 @@ class Video:
         self.video = youtube.videos().list(id=self.video_id,
                                            part='snippet,statistics,contentDetails,topicDetails'
                                            ).execute()
-        self.title = self.video['items'][0]['snippet']['title']
-        self.url = 'https://www.youtube.com/watch/' + self.video_id
-        self.view_count = int(self.video['items'][0]['statistics']['viewCount'])
-        self.like_count = int(self.video['items'][0]['statistics']['likeCount'])
+        try:
+            if not self.video['items']:
+                raise IdError
+        except IdError:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
+        else:
+            self.title = self.video['items'][0]['snippet']['title']
+            self.url = 'https://www.youtube.com/watch/' + self.video_id
+            self.view_count = int(self.video['items'][0]['statistics']['viewCount'])
+            self.like_count = int(self.video['items'][0]['statistics']['likeCount'])
 
     def __str__(self):
         """Возвращает название ютуб-видео"""
@@ -28,5 +42,6 @@ class PLVideo(Video):
     """Класс для ютуб-плейлиста"""
 
     def __init__(self, video_id: str, playlist_id: str) -> None:
+        """Экземпляр инициализируется id видео и id плейлиста."""
         super().__init__(video_id)
         self.playlist_id = playlist_id
